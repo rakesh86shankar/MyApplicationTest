45ee001420570a73af94ba77e2ee79b88b0eb908
rakesh shankar <rakesh86shankar@gmail.com>
2020-05-29 22:45:22 +0530

    To check conflict.

--- app/src/main/java/com/example/myapplicationtest/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationtest/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.v("To Test Conflict",">>>@333")
         replaceFragment(NewsPaperListFragment(),"Page1")
     }
 
---
+++ app/src/main/java/com/example/myapplicationtest/view/NewsPaperFragment.kt
@@ -0,0 +1,2 @@
+package com.example.myapplicationtest.view
+
